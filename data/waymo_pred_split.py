2013b894cb17fd2c8ca9d416eb41b33ea9259fe1
rishitoshsingh <rishitoshs@gmail.com>
2025-04-26 15:37:41 -0700

    removed waymo test scenes from testing nuscenes_waymo dataset

--- data/waymo_pred_split.py
+++ data/waymo_pred_split.py
@@ -6,6 +6,8 @@ def get_waymo_pred_split(data_root):
      split_data = []
      for split in ['train', 'val', 'test']:
           files = sorted(glob.glob(f'{data_root}/label/{split}/scene*.txt'))
+          if split == 'test':
+               files = [f for f in files if os.path.basename(f).split('-')[1].split('.')[0].isdigit()]
           scenes = [os.path.splitext(os.path.basename(x))[0] for x in files]
           split_data.append(scenes)
      return split_data
